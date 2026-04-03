be1ac7df67b4b5eb7ce712e7d071a5edd23e114f
Raleese <titaslr38@gmail.com>
2026-04-04 02:56:03 +0300

    Added a way to see if user is using AI ir rule mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,25 @@
+import { useState } from "react"
+import type { ChatMode } from "./types/chat"
 import ChatInput from "./components/ChatInput"
 import ChatWindow from "./components/ChatWindow"
 import ModeSelector from "./components/ModeSelector"
 
 function App() {
+  const [mode, setMode] = useState<ChatMode>("rule")
+
   return (
     <div className="flex flex-col min-h-screen">
       <h1 className="text-3xl font-bold underline" style={{ textAlign: "center"}}>
         Chatbot Demo
       </h1>
       <div>
-        <ModeSelector mode="ai" onModeChange={() => {}} />
+        <ModeSelector mode={mode} onModeChange={setMode} />
       </div>
       <div>
         <ChatWindow messages={[]} />
       </div>
       <div className="mt-auto">
-        <ChatInput/>
+        <ChatInput mode={mode} />
       </div>
     </div>
   )
