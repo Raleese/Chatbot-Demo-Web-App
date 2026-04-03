5f01643f8ab45880c91a395a8f13fbbb5857f05c
Raleese <titaslr38@gmail.com>
2026-04-03 21:45:20 +0300

    Updated display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import tailwindcss from "@tailwindcss/vite"
+import ChatInput from "./components/ChatInput"
+import ChatWindow from "./components/ChatWindow"
+import ModeSelector from "./components/ModeSelector"
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="flex flex-col min-h-screen">
+      <h1 className="text-3xl font-bold underline" style={{ textAlign: "center"}}>
+        Chatbot Demo
+      </h1>
+      <div>
+        <ModeSelector mode="ai" onModeChange={() => {}} />
+      </div>
+      <div>
+        <ChatWindow messages={[]} />
+      </div>
+      <div className="mt-auto">
+        <ChatInput/>
+      </div>
+    </div>
   )
 }
 
