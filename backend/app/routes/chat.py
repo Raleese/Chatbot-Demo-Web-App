c0deb7a08ce3f9f770f2325740ab315f3a83b928
Raleese <titaslr38@gmail.com>
2026-04-04 15:43:41 +0300

    Added communication with ai chatbot

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from app.models.message import ChatRequest, ChatResponse
 from app.chatbot.rule_bot import rule_bot_response
+from app.chatbot.ai_bot import ai_bot_response
 
 router = APIRouter()
 
-@router.post("/rule_chat", response_model=ChatResponse)
+@router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
     if request.mode == "rule":
         reply = rule_bot_response(request.message)
-    else:
-        reply = "AI mode is not connected yet."
+    elif request.mode == "ai":
+        reply = ai_bot_response(request.message)
 
     return ChatResponse(reply=reply)
