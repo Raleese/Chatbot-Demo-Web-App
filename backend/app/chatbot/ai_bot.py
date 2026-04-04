ffc8ea3a3d942b584c1f27d5bfeb906e9d0c903c
Titas Aleškevičius <titaslr38@gmail.com>
2026-04-04 15:44:36 +0300

    AI Gemini API and enhance backend configuration (#5)
    * changed chat_api filename and function name
    
    * changed imports
    
    * Changed requirements.txt
    
    * Added gitignore for backend
    
    * Added .env file example
    
    * Added GEMINI api connection
    
    * Added communication with ai chatbot

---
+++ backend/app/chatbot/ai_bot.py
@@ -0,0 +1,15 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+def ai_bot_response(message : str) -> str:
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=message
+    )
+
+    return response.text
