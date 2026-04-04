69525dad2bc4151dde332797538b3513f77a60bf
Raleese <titaslr38@gmail.com>
2026-04-04 12:58:06 +0300

    Implemented dark mode

--- frontend/src/components/ChatWindow.tsx
+++ frontend/src/components/ChatWindow.tsx
@@ -3,14 +3,21 @@ import MessageBubble from "./MessageBubble";
 
 type Props = {
     messages: Message[];
+    isDarkMode: boolean;
 };
 
-function ChatWindow({ messages }: Props){
+function ChatWindow({ messages, isDarkMode }: Props){
     return (
-        <div className="flex-1 h-full overflow-y-auto rounded-l border border-gray-200 bg-gray-50 p-4">
-            <div className="flex flex-col gap-4">
+        <div
+            className={`flex-1 max overflow-y-auto rounded-l border p-4 transition-colors ${
+                isDarkMode
+                    ? "border-gray-800 bg-gray-900"
+                    : "border-gray-200 bg-gray-50"
+            }`}
+        >
+            <div className="flex flex-col gap-4" style={{ height: "75vh", maxHeight: "75vh", overflowY: "auto" }}>
                 {messages.map((msg) => (
-                    <MessageBubble key={msg.id} message={msg} />
+                    <MessageBubble key={msg.id} message={msg} isDarkMode={isDarkMode} />
                 ))}
             </div>
         </div>
